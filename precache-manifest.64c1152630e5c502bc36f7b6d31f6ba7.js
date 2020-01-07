6451a6100b7ee839a9c3bda49e0bc1775f3cee03
manjilnepali <manjilnepali98@gmail.com>
2020-01-07 23:36:00 +0545

    Updates

---
+++ precache-manifest.64c1152630e5c502bc36f7b6d31f6ba7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3e684fd944e102a35569071bf89afd0",
+    "url": "monzilnepali.github.io/user-profile-react/index.html"
+  },
+  {
+    "revision": "2b66284f3137b136bef0",
+    "url": "monzilnepali.github.io/user-profile-react/static/css/main.82173515.chunk.css"
+  },
+  {
+    "revision": "11fc9660b7a58b462351",
+    "url": "monzilnepali.github.io/user-profile-react/static/js/2.7e330c6e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "monzilnepali.github.io/user-profile-react/static/js/2.7e330c6e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2b66284f3137b136bef0",
+    "url": "monzilnepali.github.io/user-profile-react/static/js/main.08d10721.chunk.js"
+  },
+  {
+    "revision": "e547ce61f7190b028c07",
+    "url": "monzilnepali.github.io/user-profile-react/static/js/runtime-main.6c8da42f.js"
+  }
+]);
