7292da893231587ea66f05cdb4d80e3663475962
manjilnepali <manjilnepali98@gmail.com>
2020-01-08 14:16:50 +0545

    Updates

---
+++ precache-manifest.20ddad13da39e6e576fed9551fc4dcfd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "743c2f2b925050d82856ce87f41bc50c",
+    "url": "/user-profile-react/index.html"
+  },
+  {
+    "revision": "5e2519587a8883088c22",
+    "url": "/user-profile-react/static/css/main.0394e43a.chunk.css"
+  },
+  {
+    "revision": "6cdc31d827dee508726a",
+    "url": "/user-profile-react/static/js/2.594449d0.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/user-profile-react/static/js/2.594449d0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5e2519587a8883088c22",
+    "url": "/user-profile-react/static/js/main.875c6610.chunk.js"
+  },
+  {
+    "revision": "e2ca16d3d6f8275a0e3a",
+    "url": "/user-profile-react/static/js/runtime-main.fc16ef27.js"
+  },
+  {
+    "revision": "7a21cbe90fd31540fb0384792fbc4a86",
+    "url": "/user-profile-react/static/media/defaultUser.7a21cbe9.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/user-profile-react/precache-manifest.15da355d47b76c2a4d2e60046f39c8a4.js"
+  "/user-profile-react/precache-manifest.20ddad13da39e6e576fed9551fc4dcfd.js"
 );
 
 self.addEventListener('message', (event) => {
